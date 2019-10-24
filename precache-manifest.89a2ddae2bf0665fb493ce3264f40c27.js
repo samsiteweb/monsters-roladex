74e33b2a78c8bf8ba38880aa2702e0f3a5da8d40
Lawal Samuel <samthedonz@gmail.com>
2019-10-23 19:23:30 -0700

    Updates

---
+++ precache-manifest.89a2ddae2bf0665fb493ce3264f40c27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd211945e674d6096de06ecfa78812d",
+    "url": "/monsters-roladex/index.html"
+  },
+  {
+    "revision": "3911c3c934d04873f69a",
+    "url": "/monsters-roladex/static/css/main.1595456f.chunk.css"
+  },
+  {
+    "revision": "ecb8c7a120110bb4b661",
+    "url": "/monsters-roladex/static/js/2.14b48efa.chunk.js"
+  },
+  {
+    "revision": "3911c3c934d04873f69a",
+    "url": "/monsters-roladex/static/js/main.a02e1e66.chunk.js"
+  },
+  {
+    "revision": "cb0c8be638fdaadb534a",
+    "url": "/monsters-roladex/static/js/runtime-main.efb3a203.js"
+  }
+]);
